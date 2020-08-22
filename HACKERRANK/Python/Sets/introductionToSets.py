0058d6c9c8d24e3247e254c5f373fc22f3670122
Sayantan Paul <belikesayantan12@gmail.com>
2020-08-22 17:34:49 +0530

    Day 12 #1
    Day 11 of 100daysofcode
    
    * Introduction to Sets

---
+++ HACKERRANK/Python/Sets/introductionToSets.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def average(array: List[int]):
+    return sum(set(array)) / len(set(array))
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
